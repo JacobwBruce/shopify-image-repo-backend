27557e48fec1d6487cca69ddc71da7aee8dd3bf6
JacobwBruce <jacobwbruce@hotmail.com>
2020-12-27 17:01:23 -0500

    hooked up uploadRoutes to server

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
 import express from 'express';
+import path from 'path';
+import uploadRoutes from './routes/uploadRoutes';
 
 const app = express();
+
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.send('API is running...');
+});
+
+app.use('/api/upload', uploadRoutes);
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
