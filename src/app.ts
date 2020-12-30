afd8c7878daac9f64e350e6f2645c682195446a5
JacobwBruce <jacobwbruce@hotmail.com>
2020-12-30 15:32:59 -0500

    Fixed user routes

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db';
 import uploadRoutes from './routes/uploadRoutes';
 import { errorHandler, notFound } from './middleware/errorMiddleware';
+import userRoutes from './routes/userRoutes';
 
 dotenv.config();
 
@@ -17,10 +18,11 @@ app.get('/', (req: express.Request, res: express.Response) => {
     res.send('API is running...');
 });
 
+app.use('/api/upload', uploadRoutes);
+app.use('/api/users', userRoutes);
+
 app.use(notFound);
 app.use(errorHandler);
-
-app.use('/api/upload', uploadRoutes);
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
