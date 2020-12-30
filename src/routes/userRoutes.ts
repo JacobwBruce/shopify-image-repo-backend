a4c53ff684632b87c492128a9249a396707adce7
JacobwBruce <jacobwbruce@hotmail.com>
2020-12-30 15:38:17 -0500

    Register User

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -8,6 +8,7 @@ import {
 import { protect } from '../middleware/authMiddleware';
 const router: Router = express.Router();
 
+router.route('/').post(registerUser);
 router.post('/login', authUser);
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
 
