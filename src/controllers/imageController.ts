93b404524bee6c96e8ac6c8d4573e9f0327d782b
JacobwBruce <jacobwbruce@hotmail.com>
2020-12-31 16:52:02 -0500

    Get User Images endpoint

--- src/controllers/imageController.ts
+++ src/controllers/imageController.ts
@@ -1,11 +1,31 @@
 import express from 'express';
+import UserRequest from '../interfaces/UserRequest';
+import Image from '../models/imageModel';
 
 // @desc    get an image by file name
-// @route   POST /api/images/:url
+// @route   GET /api/images/:url
 // @access  Public
 export const getImage = (req: express.Request, res: express.Response) => {
     const url = req.params.url;
     res.sendFile(`/images/${url}`, {
         root: './',
     });
+};
+
+// @desc    get all images
+// @route   GET /api/images/
+// @access  Public
+export const getAllImages = async (req: express.Request, res: express.Response) => {
+    const images = await Image.find({});
+
+    res.json(images.reverse());
+};
+
+// @desc    get logged in users images
+// @route   GET /api/images/user
+// @access  Private
+export const getUserImages = async (req: UserRequest, res: express.Response) => {
+    const images = await Image.find({ user: req.user!._id });
+
+    res.json(images.reverse());
 };
--- src/routes/imageRoutes.ts
+++ src/routes/imageRoutes.ts
@@ -1,7 +1,10 @@
 import express, { Router } from 'express';
-import { getImage } from '../controllers/imageController';
+import { getAllImages, getImage, getUserImages } from '../controllers/imageController';
+import { protect } from '../middleware/authMiddleware';
 const router: Router = express.Router();
 
+router.get('/user', protect, getUserImages);
 router.get('/:url', getImage);
+router.get('/', getAllImages);
 
 export default router;
