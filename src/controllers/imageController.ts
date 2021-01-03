4b9b163e8d10d93ca81138c128293b4ce0feb635
JacobwBruce <jacobwbruce@hotmail.com>
2021-01-03 14:15:18 -0500

    Delete file by id

--- src/controllers/imageController.ts
+++ src/controllers/imageController.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import UserRequest from '../interfaces/UserRequest';
 import Image from '../models/imageModel';
+import fs from 'fs';
 
 // @desc    get an image by file name
 // @route   GET /api/images/:url
@@ -28,4 +29,19 @@ export const getUserImages = async (req: UserRequest, res: express.Response) => 
     const images = await Image.find({ user: req.user!._id });
 
     res.json(images.reverse());
+};
+
+// @desc    delete an image from id
+// @route   DELETE /api/images/:id
+// @access  Private
+export const deleteImage = async (req: UserRequest, res: express.Response) => {
+    const image = await Image.findById(req.params.url);
+    if (!image) {
+        res.status(404);
+        throw new Error('Image not found');
+    } else {
+        fs.unlinkSync(`.${image.url}`);
+        await image.remove();
+        res.json({ message: 'Image deleted' });
+    }
 };
--- src/routes/imageRoutes.ts
+++ src/routes/imageRoutes.ts
@@ -1,10 +1,10 @@
 import express, { Router } from 'express';
-import { getAllImages, getImage, getUserImages } from '../controllers/imageController';
+import { deleteImage, getAllImages, getImage, getUserImages } from '../controllers/imageController';
 import { protect } from '../middleware/authMiddleware';
 const router: Router = express.Router();
 
 router.get('/user', protect, getUserImages);
-router.get('/:url', getImage);
+router.route('/:url').get(getImage).delete(protect, deleteImage);
 router.get('/', getAllImages);
 
 export default router;
